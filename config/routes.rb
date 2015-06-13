592d25d77b3d50a8a45cda5a2a3f62b862cd4ddb
石本崚 <ryoishi0801@icloud.com>
2015-06-13 16:48:52 +0900

    準備

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,7 @@ Rails.application.routes.draw do
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 get 'items/:id' => 'items#show'
 get 'items/' => 'items#index'
+get 'items/new' => 'items#new'
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
 
