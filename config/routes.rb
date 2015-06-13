1c2b99070bd68216c1884edf7312a1a044aaeab9
石本崚 <ryoishi0801@icloud.com>
2015-06-13 16:18:38 +0900

    show actionの追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -10,7 +11,7 @@ Rails.application.routes.draw do
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
-
+get 'items/:id' => 'items#show'
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
 
